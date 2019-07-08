e085d3a62e33b06f20c00101d164f110d826c378
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-07-08 16:51:39 +0700

    Add: simple post with form data

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+func checkPanic(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func login(voucherCode string) {
+	calledURL := "http://10.0.2.1:8002/index.php?zone=cpzone"
+	formData := url.Values{}
+	formData.Set("auth_user", "")
+	formData.Set("auth_pass", "")
+	formData.Set("auth_voucher", voucherCode)
+	formData.Set("redirurl", "http://www.apple.com/library/test/success.html")
+	formData.Set("accept", "Accept")
+
+	response, err := http.PostForm(calledURL, formData)
+	checkPanic(err)
+	responseBody, err := ioutil.ReadAll(response.Body)
+	checkPanic(err)
+	responseBodyData := string(responseBody)
+	fmt.Println(strings.Contains(responseBodyData, "success"))
+}
+
+func main() {
+	voucherCode := flag.String("voucher_code", "", "Set voucher code when you bought something at Cafe")
+	flag.Parse()
+	if len(*voucherCode) > 0 {
+		login(*voucherCode)
+	}
+}
